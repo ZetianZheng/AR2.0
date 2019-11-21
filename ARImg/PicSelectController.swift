5f11265980cf51bafddbf280029a4b20dc54d18b
sxy <sxy@SEAS13942.local>
2019-11-21 17:29:28 -0500

    first commit

---
+++ ARImg/PicSelectController.swift
@@ -0,0 +1,30 @@
+//
+//  PicSelectController.swift
+//  ARImg
+//
+//  Created by sxy on 11/21/19.
+//  Copyright © 2019 sxy. All rights reserved.
+//
+
+import UIKit
+
+class PicSelectController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ARImg/Picture.swift
@@ -0,0 +1,9 @@
+//
+//  Picture.swift
+//  ARImg
+//
+//  Created by sxy on 11/20/19.
+//  Copyright © 2019 sxy. All rights reserved.
+//
+
+import Foundation
